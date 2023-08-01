45316e2755dd088fea21d30106e6c2437485391f
zeuz <zeuz@zeuzs-iMac.local>
2023-08-01 16:28:56 -0400

    set up exercise model

--- server/models/Exercise.js
+++ server/models/Exercise.js
@@ -1,7 +1,15 @@
 const { Schema, model } = require('mongoose');
 
 const exerciseSchema = new Schema({
-  
+    type: {
+        type: String,
+    },
+    duration: {
+        type: Number,
+    },
+    intensity: {
+        type: String
+    }
 });
 
 module.exports = exerciseSchema;
