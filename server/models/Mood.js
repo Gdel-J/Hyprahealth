7eaa165021b88bf46b31568c76753a987d28f9f3
zeuz <zeuz@zeuzs-iMac.local>
2023-08-01 16:30:03 -0400

    set up mood model

--- server/models/Mood.js
+++ server/models/Mood.js
@@ -1,7 +1,19 @@
 const { Schema, model } = require('mongoose');
 
 const moodSchema = new Schema({
-  
+    type: {
+        type: String
+    },
+    severity: {
+        type: String
+    },
+    time: {
+        type: Date,
+        default: Date.now
+    },
+    other: {
+        type: String
+    }
 });
 
 module.exports = moodSchema;
