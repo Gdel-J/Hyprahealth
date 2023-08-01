cd84ecd6595c9cb247135c285c9a6020395a8bd0
zeuz <zeuz@zeuzs-iMac.local>
2023-08-01 16:27:36 -0400

    set up meal model

--- server/models/Meal.js
+++ server/models/Meal.js
@@ -1,7 +1,18 @@
 const { Schema, model } = require('mongoose');
 
 const mealSchema = new Schema({
-  
+    item: {
+        type: String,
+    },
+    quantity: {
+        type: Number,
+        min: 0,
+        default: 0
+      },
+    time: {
+        type: Date,
+        default: Date.now
+    }
 });
 
 module.exports = mealSchema;
